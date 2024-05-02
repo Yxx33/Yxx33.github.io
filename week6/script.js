98cf9b0a6f909b745c0ca2c3623ba8aa68db3625
Yxx33 <s3934536@student.rmit.edu.au>
2024-05-02 15:10:44 +1000

    added image

--- week6/script.js
+++ week6/script.js
@@ -1,4 +1,33 @@
-const myHeading = document.querySelector("h1");
-console. log(myHeading);
-console. log(myHeading.textContent);
-myHeading.textContent="new heading";
+// const myHeading = document.querySelector("h1");
+// console. log(myHeading);
+// console. log(myHeading.textContent);
+// myHeading.textContent="new heading";
+
+// const myPara.forEach(colorMe);
+
+// function colorMe(item)
+
+// const myPara = document.querySelectorAll("p");
+// console.log(myPara);
+// const myColors = ["purple-box", "coral-box, "lime-box",];
+
+// for (let i = 0; i < 4; i++){
+//     myPara[i].classList
+// }
+
+const myImg = document.querySelector("#cat-image");
+console.log(myImg);
+
+const myButton = document.querySelector("#my-button");
+console.log(myButton);
+
+myButton.addEventListener("click", toggleMe);
+
+function toggleMe(){
+    const myImg = document.querySelector("#cat-image");
+    // console.log(myImg);
+    myImg.classList.toggle("round")
+     const myDiv = document.querySelector(".outer");
+     myDiv.classList.toggle("coarl-box");
+}
+
