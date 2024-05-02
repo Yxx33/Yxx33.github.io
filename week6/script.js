76eb4bb8c109a2443aeae3a11722bfdb2b10f5fe
Yxx33 <s3934536@student.rmit.edu.au>
2024-05-02 16:24:13 +1000

    added new

--- week6/script.js
+++ week6/script.js
@@ -15,19 +15,84 @@
 //     myPara[i].classList
 // }
 
+// const myImg = document.querySelector("#cat-image");
+// console.log(myImg);
+
+// const myButton = document.querySelector("#my-button");
+// console.log(myButton);
+
+// myButton.addEventListener("click", toggleMe);
+
+// function toggleMe(){
+//     const myImg = document.querySelector("#cat-image");
+ // console.log(myImg);
+//     myImg.classList.toggle("round")
+//     const myDiv = document.querySelector("h1");
+//     myTitle.textContent = myImg.dataset.catname;
+    //  const myDiv = document.querySelector(".outer");
+    //  myDiv.classList.toggle("coarl-box");
+// }
+
+// const myButton = document.querySelector("#my-button");
+// console.log(myButton);
+
+// myButton.addEventListener("click", chooseTopic);
+
+// function chooseTopic()
+// {
+//     const myProjects = document.querySelectorAll("p");
+//     myProjects.forEach(changeColor);
+//     function changeColor(item)
+//     {
+//         if(item.dataset.topic==="web")
+//         {
+//             item.classList.add("purple-box");
+//         }
+//         else if (item.dataset.topic==="sound")
+//         {
+//             item.classList.add("coral-box");
+//         }
+//         else if (item.dataset.topic==="games")
+//         {
+//             item.classList.add("lime-box");
+//         }
+//     }
+// }
+
+// const myProjects = document.querySelectorAll("p");
+// myProjects.forEach(changeColor);
+// function changeColor(item)
+// {
+//     if(item.dataset.topic==="indian")
+//     {
+//         item.classList.add("purple-box");
+//     }
+//     else if (item.dataset.topic==="mexican")
+//     {
+//         item.classList.add("coral-box");
+//     }
+//     else if (item.dataset.topic==="Thai")
+//     {
+//         item.classList.add("lime-box");
+//     }
+// }
+
+const courseName = "OART1013";
+const myHeading = document.querySelector("h1");
+console.log(myHeading);
+myHeading.innerHTML =`new heading <span class="coral-box"> ${courseName} </span>`;
+
+// myHeading.textContent = "new heading";
+
 const myImg = document.querySelector("#cat-image");
-console.log(myImg);
 
-const myButton = document.querySelector("#my-button");
-console.log(myButton);
+myImg.addEventListener("mouseover",makeItRound);
+myImg.addEventListener("mouseout",makeItSquare);
 
-myButton.addEventListener("click", toggleMe);
-
-function toggleMe(){
-    const myImg = document.querySelector("#cat-image");
-    // console.log(myImg);
-    myImg.classList.toggle("round")
-     const myDiv = document.querySelector(".outer");
-     myDiv.classList.toggle("coarl-box");
+function makeItRound(){
+    myImg.classList.add("add");
 }
 
+function makeItSquare(){
+    myImg.classList.remove("round");
+}
