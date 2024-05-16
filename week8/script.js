a9668486557fa19be73a97c1b9f0cb3b955bcd84
Yxx33 <s3934536@student.rmit.edu.au>
2024-05-16 15:17:20 +1000

    added

--- week8/script.js
+++ week8/script.js
@@ -44,3 +44,37 @@ function toggleSound()
 }
 
 //---------------------------------------------------------------------------------
+
+// the following code allows us to set a progres bar for video
+
+myVideo.addEventListener("timeupdate", updateProgressBar);
+
+const progressBarFill = document.querySelector("#progress-bar-fill")
+console.log(progressBarFill);
+
+function updateProgressBar(){
+    const progress =(myVideo.currentTime / myVideo.duration) * 100;
+    console.log(progress);
+    progressBarFill.style.width = progress + "%";
+}
+
+//---------------------------------------------------------------------------------
+
+// the following code allows us to nevigate through different timestamps
+
+const step1Button = document.querySelector("#step-1-button")
+console.log(step1Button);
+step1Button.addEventListener("click", gotoStep1);
+
+function gotoStep1(){
+    myVideo.currentTime = 17.0
+}
+
+const step2Button = document.querySelector("#step-2-button")
+console.log(step2Button);
+step2Button.addEventListener("click", gotoStep2);
+
+function gotoStep2(){
+    myVideo.currentTime = 52.0
+}
+//---------------------------------------------------------------------------------
