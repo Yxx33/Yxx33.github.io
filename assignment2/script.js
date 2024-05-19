4c5bf9d43a101ea94ef7cda5719411552f39957b
Yxx33 <s3934536@student.rmit.edu.au>
2024-05-19 23:40:07 +1000

    added assignment2

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,8 +1,13 @@
-const video = document.querySelector("#custom-video-player");
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+
+const video = document.querySelector("#my-video");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
 video.removeAttribute("controls");
+
+
 // playPauseBtn.addEventListener("click", togglePlayPause);
 video.addEventListener("timeupdate", updateProgressBar);
 function togglePlayPause() {
@@ -18,26 +23,25 @@ function updateProgressBar() {
   const value = (video.currentTime / video.duration) * 100;
   progressBar.style.width = value + "%";
 }
-// Add other functionalities here
 
 //---------------------------------------------------------------------------------
-// The following code block allowws us tp mute and unmute video on a click
+// The following code block allows us to mute or unmute a video on a click
 
 const muteUnmuteButton = document.querySelector("#mute-unmute-button");
 console.log(muteUnmuteButton);
 
 muteUnmuteButton.addEventListener("click", toggleSound);
 
-function toggleSound()
-{
-    if (myVideo.muted) {
-        muteUnmuteButton.style.backgroundColor = "#d5cea3";
-        myVideo.muted = false;
-     } else {
-        muteUnmuteButton.style.backgroundColor = "#75715d";
-        myVideo.muted = true;
-     }
+function toggleSound() {
+  if (myVideo.muted) {
+    muteUnmuteButton.style.backgroundColor = "#d5cea3";
+    myVideo.muted = false;
+  } else {
+    muteUnmuteButton.style.backgroundColor = "#78755f";
+    myVideo.muted = true;
+  }
 }
+
 //---------------------------------------------------------------------------------
 // following code allows us to control volume of the video
 const increaseVolumeButton = document.querySelector("#increase-volume-button");
@@ -65,8 +69,18 @@ function decreaseVolume(){
 }
 
 myVideo.addEventListener("volumechange", updateVolume);
-
 function updateVolume(){
     console.log("current volume is", myVideo.volume);
 }
+
 //---------------------------------------------------------------------------------
+// Toggle repeat function
+function toggleRepeat() {
+    const video = document.getElementById('my-video');
+    const repeatImg = document.getElementById('repeat-img');
+  
+    video.loop = !video.loop;
+    repeatImg.src = video.loop
+      ? 'https://img.icons8.com/ios-glyphs/30/repeat-on.png' // URL for "repeat on" icon
+      : 'https://img.icons8.com/ios-glyphs/30/repeat.png'; // URL for "repeat off" icon
+}
