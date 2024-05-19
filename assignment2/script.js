206182000ffe939fb49c9f49b168611a8bd3ebf9
Yxx33 <s3934536@student.rmit.edu.au>
2024-05-19 22:12:48 +1000

    added assignment2

--- assignment2/script.js
+++ assignment2/script.js
@@ -19,3 +19,54 @@ function updateProgressBar() {
   progressBar.style.width = value + "%";
 }
 // Add other functionalities here
+
+//---------------------------------------------------------------------------------
+// The following code block allowws us tp mute and unmute video on a click
+
+const muteUnmuteButton = document.querySelector("#mute-unmute-button");
+console.log(muteUnmuteButton);
+
+muteUnmuteButton.addEventListener("click", toggleSound);
+
+function toggleSound()
+{
+    if (myVideo.muted) {
+        muteUnmuteButton.style.backgroundColor = "#d5cea3";
+        myVideo.muted = false;
+     } else {
+        muteUnmuteButton.style.backgroundColor = "#75715d";
+        myVideo.muted = true;
+     }
+}
+//---------------------------------------------------------------------------------
+// following code allows us to control volume of the video
+const increaseVolumeButton = document.querySelector("#increase-volume-button");
+console.log(increaseVolumeButton);
+
+increaseVolumeButton.addEventListener("click", increaseVolume);
+
+// volume ranges from 0 to 1 with increment of 0.1
+function increaseVolume(){
+    if(myVideo.volume < 0.9){
+        myVideo.volume += 0.1;
+    }
+}
+
+const decreaseVolumeButton = document.querySelector("#decrease-volume-button");
+console.log(decreaseVolumeButton);
+
+decreaseVolumeButton.addEventListener("click", decreaseVolume);
+
+// volume ranges from 0 to 1 with increment of 0.1
+function decreaseVolume(){
+    if(myVideo.volume > 0.11){
+        myVideo.volume -= 0.1;
+    }
+}
+
+myVideo.addEventListener("volumechange", updateVolume);
+
+function updateVolume(){
+    console.log("current volume is", myVideo.volume);
+}
+//---------------------------------------------------------------------------------
